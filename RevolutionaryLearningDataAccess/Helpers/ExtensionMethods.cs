f0a84be95add7fb32a136189b358b6ceb1edb296
jblanchette <jblanchette@co-construct.com>
2018-05-13 14:40:45 -0400

    added extension methods

---
+++ RevolutionaryLearningDataAccess/Helpers/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using DTOCollection;
+
+namespace RevolutionaryLearningDataAccess
+{
+	public static class ExtensionMethods
+	{
+		public static T Convert<T>(this T obj, object convertFrom) where T : DTOBase
+		{
+			IMapper mapper = Mapper.Configuration.CreateMapper();
+
+			return mapper.Map<T>(convertFrom);
+		}
+
+		public static List<T> Convert<T>(this List<T> list, object convertFromList)
+		{
+			IMapper mapper = Mapper.Configuration.CreateMapper();
+
+			return mapper.Map<List<T>>(convertFromList);
+		}
+	}
+}
